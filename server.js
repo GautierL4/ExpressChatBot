f34d9659fbba7b588ba194f823f0b63281352cc2
GautierL4 <gautier.laurent4@gmail.com>
2019-09-25 12:22:07 +0200

    Récupération parametre dans la route

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.get('/', function(req,res){
     res.send('Hello World')
 })
 
+app.get('/hello', function(req,res){
+    if(req.query.nom){
+        res.send('Bonjour, ' + req.query.nom)
+    }
+    else{
+        res.send('Quel est votre nom ?')
+    }
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT , function() {
     console.log(`App listening on port ${ PORT }`)
